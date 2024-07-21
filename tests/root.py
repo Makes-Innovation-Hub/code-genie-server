5e97d90228109b89071ba2c91bb704730534e7cc
Mohamad Shaheen <mhmd.shahin@outlook.com>
2024-07-21 19:55:30 +0300

    Added Server Deployment and Testing
    There were no need to create a branch for that matter, it is mostly a work that isn't code related

--- tests/root.py
+++ tests/root.py
@@ -1,10 +1,24 @@
 from fastapi.testclient import TestClient
+from dotenv import load_dotenv
 from server import app
+import requests
+import os
+
+load_dotenv()
 
 client = TestClient(app)
+
+server_url = os.getenv('SERVER_URL')
 
 
 def test_root():
     response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == 'Hello from FastAPI server'
+
+
+def test_remote_root():
+    # Note that we used requests.get instead of client.get
+    response = requests.get(server_url)
     assert response.status_code == 200
     assert response.json() == 'Hello from FastAPI server'
