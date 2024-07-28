7069433fc90c8f67283cbe604e287418dacb7463
hasanMasalha <106591427+hasanMasalha@users.noreply.github.com>
2024-07-28 16:24:48 +0300

    logging added, need update, this is not a final version

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-
+from logging_pac.logging_setup import logger
 app = FastAPI()
 
 
 @app.get('/')
 async def root():
+    logger.info("this is a server test")
     return 'Hello from FastAPI server'
