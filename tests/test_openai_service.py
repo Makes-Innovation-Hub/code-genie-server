8b0129bdd7bdab4bd4f41fa6f6fc93d374c8c2fb
yishain11 <yishain11@walla.co.il>
2024-07-28 17:56:15 +0300

    added test to basic question generation endpoint

--- tests/test_openai_service.py
+++ tests/test_openai_service.py
@@ -9,3 +9,14 @@ def test_openai_endpoint():
     assert isinstance(
         response.text, str) == True
     assert response.text.lower().replace('"', '') == "hello"
+    
+def test_basic_question_generation():
+    server_url = os.getenv("SERVER_URL")
+    url = f"{server_url}/question/basic"
+    response = requests.post(url,json={"topic":"python"})
+    assert response.status_code == 200
+    answer = response.json()
+    assert "question" in answer
+    assert "answer" in answer
+    assert "python" in answer["question"].lower()
+    
