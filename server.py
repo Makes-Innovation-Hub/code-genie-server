83da52de7d15e9802b977934334cd52c6f378bcf
hasanMasalha <106591427+hasanMasalha@users.noreply.github.com>
2024-07-28 19:48:06 +0300

    get question with four answers updated

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
-
+from generateQuestions import generate_question_4_answers
 app = FastAPI()
 
 
 @app.get('/')
 async def root():
     return 'Hello from FastAPI server'
+
+# @app.get('/question_four_answers')
+# async def get_question_four_answers(topic: str):
+#     try:
+#         answer = generate_question_4_answers.get_question_and_answers(topic)
+#         print(answer)
+#         return answer
+#     except Exception as e:
+#         print(e)
+#         return e
