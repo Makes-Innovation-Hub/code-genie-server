4330e2005276d43a2eb23155cfbe633c065cacf3
BaselAmin712 <baselamin712@gmail.com>
2024-07-29 15:34:32 +0300

    separate the .env into 2 files .env.prod and .env.dev using global variables for the enviroment status

--- db/connection_credentials.py
+++ db/connection_credentials.py
@@ -1,9 +1,16 @@
+import argparse
 from urllib.parse import quote_plus
 from dotenv import load_dotenv
+from globals import globals
 import os
 
-load_dotenv()
+# Load the appropriate .env file
+if globals.env_status == "dev":
+    load_dotenv('.env.dev')
+else:
+    load_dotenv('.env.prod')
 
 mongodb_username = quote_plus(os.getenv('MONGODB_USERNAME'))
 mongodb_password = quote_plus(os.getenv('MONGODB_PASSWORD'))
-mongodb_url = f'mongodb+srv://{mongodb_username}:{mongodb_password}@telegrambot.ztcvwep.mongodb.net/?retryWrites=true&w=majority&appName=telegrambot'
+mongodb_host = os.getenv('MONGODB_HOST')
+mongodb_url = f'mongodb+srv://{mongodb_username}:{mongodb_password}@{mongodb_host}'
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import argparse
 import uvicorn
+from globals import globals
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from routes import basic_db_functions_route, openai_route
 
@@ -16,14 +18,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="FastAPI server")
     parser.add_argument('--env', type=str, default='dev', help='Environment (choices: dev, prod, default: dev)')
     args = parser.parse_args()
+    globals.env_status = args.env
 
-    env = args.env
-    if env not in ["dev", "prod"]:
+    if globals.env_status not in ["dev", "prod"]:
         raise ValueError("Invalid environment! Choose either 'dev' or 'prod'.")
 
-    print(f"Starting server in {env} environment")
+     # Load the appropriate .env file
+    if globals.env_status == "dev":
+        load_dotenv('.env.dev')
+    else:
+        load_dotenv('.env.prod')
 
-    if env == "dev":
+    print(f"Starting server in {globals.env_status} environment")
+
+    if globals.env_status == "dev":
         uvicorn.run(app, host="127.0.0.1", port=8002)
     else:
         uvicorn.run(app, host="127.0.0.1", port=8001)
