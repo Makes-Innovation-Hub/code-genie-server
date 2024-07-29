540642b8274d9b7ca5b3f7f40c01bffe35458172
BaselAmin712 <baselamin712@gmail.com>
2024-07-29 10:30:19 +0300

    add the ability to run the server with command line arguments - specificly env

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import argparse
+import uvicorn
 from fastapi import FastAPI
 from routes import basic_db_functions_route, openai_route
 
@@ -9,3 +11,20 @@ async def root():
 
 app.include_router(basic_db_functions_route.router, prefix='/test')
 app.include_router(openai_route.router, prefix='/question')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="FastAPI server")
+    parser.add_argument('--env', type=str, default='dev', help='Environment (choices: dev, prod, default: dev)')
+    args = parser.parse_args()
+
+    env = args.env
+    if env not in ["dev", "prod"]:
+        raise ValueError("Invalid environment! Choose either 'dev' or 'prod'.")
+
+    print(f"Starting server in {env} environment")
+
+    if env == "dev":
+        uvicorn.run(app, host="127.0.0.1", port=8002)
+    else:
+        uvicorn.run(app, host="127.0.0.1", port=8001)
+    
