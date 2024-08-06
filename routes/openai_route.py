e6c218addba9b9f59a253a7c253c9eb8e6f83dcc
hasanMasalha <106591427+hasanMasalha@users.noreply.github.com>
2024-08-06 12:38:23 +0300

    confict resolved with some updates3

--- routes/openai_route.py
+++ routes/openai_route.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request, Response
-from logging_pac.logging_setup import logger, RequestIDMiddleware,log_request_handling
+from logging_packages.logging_setup import logger, RequestIDMiddleware,log_request_handling
 from services.openai_service import get_chat_completion, get_question_and_answer
 from data_types.openai_req_types import GenQuestionBody as GenBody
 
@@ -13,15 +13,15 @@ async def gen_basic_question(body:GenBody, request: Request):
         log_request_handling(request_id, "this is test from the basic question")
     except Exception as e:
         print(e)
-        response.status_code = 400
         return e
       
 @router.post('/generate')
-async def gen_question(body:GenBody, response: Response):
+async def gen_question(body:GenBody, response: Response, request: Request):
     topic = body.topic
     difficulty = body.difficulty
     answers_num = body.answers_num
     try:
+        request_id = request.state.request_id
         answer = get_question_and_answer(topic,difficulty,answers_num)
         log_request_handling(request_id, "this is test from the basic question")
         return answer
