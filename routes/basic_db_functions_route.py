a9375fcff0eca372a498a144c4f3046609b4fe42
ayaabb <ayaabbas011@gmail.com>
2024-08-11 20:10:03 +0300

    restored route

--- routes/basic_db_functions_route.py
+++ routes/basic_db_functions_route.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Form
-from data_access_layer import basic_db_functions,questions_to_user
-from globals import globals
+from data_access_layer import basic_db_functions
 router = APIRouter()
 
 @router.get('/test')
@@ -13,12 +12,3 @@ async def store_rand_num_in_db():
         raise e
 
 
-@router.post('/store-data/')
-async def store_data(question: str = Form(...), answer: str = Form(...),topic: str = Form(...),
-                     explanation: str = Form(...), difficulty: str = Form(...),
-                     user_name: str = Form(...), user_id: str = Form(...)):
-
-    response = questions_to_user.store_data(question=question, topic=topic,answer=answer, explanation=explanation,
-                                            difficulty=difficulty, user_name=user_name, user_id=user_id,client=globals.mongo_client)
-
-    return response
