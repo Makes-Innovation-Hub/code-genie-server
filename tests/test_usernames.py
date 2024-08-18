ddbecc600a4d9632af0f63565f12e4eccf2c98bd
Mohamad Shaheen <mhmd.shahin@outlook.com>
2024-08-18 17:07:57 +0300

    Added saving username support

---
+++ tests/test_usernames.py
@@ -0,0 +1,21 @@
+import pytest
+from fastapi import HTTPException
+from data_access_layer.usernames_dal import store_username, check_and_delete_username
+
+def test_store_username_success():
+    username = 'test_username ' * 5
+    response = store_username(username=username)
+    assert response['username'] == username
+    assert response['available'] is True
+    assert check_and_delete_username(username=username)
+
+def test_store_username_failure():
+    username = 'test_username ' * 5
+    store_username(username=username)
+
+    with pytest.raises(HTTPException) as info:
+        store_username(username=username)
+
+    assert info.value.status_code == 409
+    assert info.value.detail == 'Username already exists'
+    check_and_delete_username(username=username)
