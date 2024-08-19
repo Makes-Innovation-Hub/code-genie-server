401aacbdad0352213884f150d4ab8bc74fe5777e
BaselAmin712 <baselamin712@gmail.com>
2024-08-19 15:11:28 +0300

    renaming files

--- data_access_layer/users_challenge_db_functions.py
+++ data_access_layer/users_challenge_db_functions.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException
 from data_access_layer.setup_mongodb import setup_mongodb
 
 def store_username(username: str, client=None):
-    collection = setup_mongodb(client, 'usernames')
+    collection = setup_mongodb(client, 'users_challenge')
     db_username = collection.find_one({'username': username})
 
     if db_username:
@@ -18,7 +18,7 @@ def store_username(username: str, client=None):
     return username_data
 
 def check_and_delete_username(username: str, client=None):
-    collection = setup_mongodb(client, 'usernames')
+    collection = setup_mongodb(client, 'users_challenge')
     db_username = collection.find_one({'username': username})
 
     if not db_username:
